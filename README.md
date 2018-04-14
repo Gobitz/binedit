af71c915cae6d9394bdb6c5e940cbe8f306a03e7
Gobitz <34771985+Gobitz@users.noreply.github.com>
2018-04-14 16:25:01 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # binedit
 Small Python library to easily edit binary data in raw str form
+
+```Python
+#Examples
+>>> hex2int("ff")
+255
+
+>>> b=Bin(128)
+>>> b.get
+'10000000'
+>>> b.add(127)
+>>> b.get
+'1000000001111111'
+>>> b.toint()
+32895
+>>> b.tohex()
+'807f'
+```
--- test_binedit.py
+++ test_binedit.py
@@ -1,5 +1,6 @@
-from . import *
+from __init__ import *
 import unittest
+from random import *
 
 class Binedittest(unittest.TestCase):
     def test_israwbin(self):
@@ -14,6 +15,31 @@ class Binedittest(unittest.TestCase):
         b=Bin(351)
         b.bytefill()
         self.assertEqual(b.get,"0000000101011111")
+
+    def test_set(self):
+        b=Bin(36)
+        b.set(255)
+        self.assertEqual(b.get,"11111111")
+
+    def test_add(self):
+        pass
+
+    def test_toint(self):
+        x=randrange(1000)
+        b=Bin(x)
+        self.assertEqual(x,b.toint())
+
+    def test_tohex(self):
+        b=Bin(3682)
+        self.assertEqual(b.tohex(),"e62")
+
+    def test_tobytes(self):
+        b=Bin("Python")
+        self.assertEqual(b.tobytes(),b"Python")
+
+    def test_tostr(self):
+        b=Bin(88482574266222)
+        self.assertEqual(b.tostr(),"Python")
 
 if __name__=="__main__":
     unittest.main()
